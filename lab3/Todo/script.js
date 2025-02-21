7b5544c9366dfc9011b4733d54ea989e43697074
Nursnapovyshku <nu_orynbassarov@kbtu.kz>
2025-02-21 11:00:12 +0500

    comment

--- lab3/Todo/script.js
+++ lab3/Todo/script.js
@@ -5,6 +5,7 @@ const todoItemsList = document.querySelector('.todo-items');
 let todos = [];
 
 todoForm.addEventListener('submit', function(event) {
+    //if(!todoInput.value) return alert("Введите текст");
     event.preventDefault();
     addTodo(todoInput.value);
 });
@@ -24,14 +25,13 @@ function addTodo(item) {
 }
 
 function renderTodos(todos) {
-    todoItemsList.innerHTML = '';
+    todoItemsList.innerHTML = ''; 
 
     todos.forEach(function(item) {
-        const checked = item.completed ? 'checked': null;
+        const checked = item.completed ? 'checked' : null;
         const li = document.createElement('li');
 
         li.setAttribute('class', 'item');
-
         li.setAttribute('data-key', item.id);
 
         if (item.completed === true) {
@@ -40,8 +40,8 @@ function renderTodos(todos) {
         li.innerHTML = `
             <input type="checkbox" class="checkbox" ${checked}>
             ${item.name}
-            <button class="delete-button"><i class="fas fa-trash fa-2x" id = "trash"></i></button>
-            `;
+            <button class="delete-button"><i class="fas fa-trash fa-2x" id="trash"></i></button>
+        `;
 
         todoItemsList.append(li);
     });
@@ -56,23 +56,26 @@ function getFromLocalStorage() {
     const reference = localStorage.getItem('todos');
 
     if (reference) {
-      todos = JSON.parse(reference);
-      renderTodos(todos);
+        todos = JSON.parse(reference);
+        renderTodos(todos);
     }
-  }
+}
 
 getFromLocalStorage();
 
 todoItemsList.addEventListener('click', function(event) {
     if (event.target.type === 'checkbox') {
-      toggle(event.target.parentElement.getAttribute('data-key'));
+        toggle(event.target.parentElement.getAttribute('data-key'));
     }
 
     if (event.target.classList.contains('delete-button')) {
-      deleteTodo(event.target.parentElement.getAttribute('data-key'));
+        deleteTodo(event.target.parentElement.getAttribute('data-key'));
     }
 });
 
+//.addEventListener('click', function() {
+   // clearAllTodos();
+//});
 function toggle(id) {
     todos.forEach(function(item) {
         if (item.id == id) {
@@ -82,11 +85,17 @@ function toggle(id) {
 
     addToLocalStorage(todos);
 }
-  
+
 function deleteTodo(id) {
     todos = todos.filter(function(item) {
         return item.id != id;
     });
 
     addToLocalStorage(todos);
+}
+
+
+function clearAllTodos() {
+    todos = [];  
+    addToLocalStorage(todos);  
 }
